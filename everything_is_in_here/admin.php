a8ac4196bf32fc99626abdd01c7e31859f33f939
GiraffeKOOOO <s5074177@bournemouth.ac.uk>
2018-07-26 14:10:42 +0100

    started on the php scripts, added a localhost db connection script, and a login for staff, so staff can now login and get redirected to view the players that signed up to a game

---
+++ everything_is_in_here/admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>admin-view</title>
+</head>
+<body>
+	<div id="main-container">
+		<h1>Signup List</h1>
+	</div>
+</body>
+</html>
---
+++ everything_is_in_here/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>game-signup</title>
+	<link rel="stylesheet" type="text/css" href="CSS/theme.css">
+    <script src="sidemenuanims.js"></script>
+</head>
+<body>
+	<div id="main-container">
+		<h1 id="select">SELECT A GAME</h1>
+		<h3 id="select">time now: 00:00</h3>
+			<div id="image-container1">
+                <span id="table">
+                    <table id="text-over">
+                        <tr>
+                            <td>Game:</td>
+                            <td>Nidhogg</td>
+                        </tr>
+                        <tr>
+                            <td>Game mode:</td>
+                            <td>1 vs 1</td>
+                        </tr>
+                        <tr>
+                            <td>Start time:</td>
+                            <td>21:00</td>
+                        </tr>
+                        <tr>
+                            <td>Signup deadline:</td>
+                            <td>20:30</td>
+                        </tr>
+                    </table>
+                </span>
+                <span class="sidemenu" id="sidemenuid1">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav1()">&times;</a>
+                    <p>enter your username below</p>
+                    <input type="text" name="user-name" placeholder="Your username" id="username-field">
+                    <input type="submit" value="submit" id="submit-btn">
+                </span>
+                <div id="button-container">
+                    <button class="signup-btn" onclick="openNav1()"><span>Sign Up</span></button>
+                </div>
+		    </div>
+
+            <div id="image-container2">
+                <span id="table">
+                    <table id="text-over">
+                        <tr>
+                            <td>Game:</td>
+                            <td>Counter-Strike: Global Offensive</td>
+                        </tr>
+                        <tr>
+                            <td>Game mode:</td>
+                            <td>Gun Game</td>
+                        </tr>
+                        <tr>
+                            <td>Start time:</td>
+                            <td>22:00</td>
+                        </tr>
+                        <tr>
+                            <td>Signup deadline:</td>
+                            <td>21:30</td>
+                        </tr>
+                    </table>
+                </span>
+                <span class="sidemenu" id="sidemenuid2">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav2()">&times;</a>
+                    <p>enter your username below</p>
+                    <input type="text" name="user-name" placeholder="Your username" id="username-field">
+                    <input type="submit" value="submit" id="submit-btn">
+                </span>
+                <div id="button-container">
+                    <button class="signup-btn" onclick="openNav2()"><span>Sign Up</span></button>
+                </div>
+            </div>
+
+            <div id="image-container3">
+                <span id="table">
+                    <table id="text-over">
+                        <tr>
+                            <td>Game:</td>
+                            <td>Team Fortress 2</td>
+                        </tr>
+                        <tr>
+                            <td>Game mode:</td>
+                            <td>Capture Point</td>
+                        </tr>
+                        <tr>
+                            <td>Start time:</td>
+                            <td>14:00</td>
+                        </tr>
+                        <tr>
+                            <td>Signup deadline:</td>
+                            <td>13:40</td>
+                        </tr>
+                    </table>
+                </span>
+                <span class="sidemenu" id="sidemenuid3">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav3()">&times;</a>
+                    <p>enter your username below</p>
+                    <input type="text" name="user-name" placeholder="Your username" id="username-field">
+                    <input type="submit" value="submit" id="submit-btn">
+                </span>
+                <div id="button-container">
+                    <button class="signup-btn" onclick="openNav3()"><span>Sign Up</span></button>
+                </div>
+            </div>
+
+            <div id="image-container4">
+                <span id="table">
+                    <table id="text-over">
+                        <tr>
+                            <td>Game:</td>
+                            <td>Golf With Your Friends</td>
+                        </tr>
+                        <tr>
+                            <td>Game mode:</td>
+                            <td>Free For All</td>
+                        </tr>
+                        <tr>
+                            <td>Start time:</td>
+                            <td>18:00</td>
+                        </tr>
+                        <tr>
+                            <td>Signup deadline:</td>
+                            <td>17:50</td>
+                        </tr>
+                    </table>
+                </span>
+                <span class="sidemenu" id="sidemenuid4">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav4()">&times;</a>
+                    <p>enter your username below</p>
+                    <input type="text" name="user-name" placeholder="Your username" id="username-field">
+                    <input type="submit" value="submit" id="submit-btn">
+                </span>
+                <div id="button-container">
+                    <button class="signup-btn" onclick="openNav4()"><span>Sign Up</span></button>
+                </div>
+            </div> 
+	</div>
+</body>
+</html>
